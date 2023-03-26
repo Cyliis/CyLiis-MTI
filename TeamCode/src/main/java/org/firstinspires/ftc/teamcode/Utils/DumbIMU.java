06f728f7f9e7a454c63c8638dd6b3c5e6cea662c
BuildTools <unconfigured@null.spigotmc.org>
2023-03-26 16:47:48 +0300

    mamata thread

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/DumbIMU.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/DumbIMU.java
@@ -2,41 +2,58 @@ package org.firstinspires.ftc.teamcode.Utils;
 
 
 import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.IMU;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.YawPitchRollAngles;
+import org.firstinspires.ftc.teamcode.OpMode.OpMode;
 
-public class DumbIMU implements IRobotModule
+public class DumbIMU
 {
     IMU imu;
 
     RevHubOrientationOnRobot orientationOnRobot;
 
-    public double heading;
+    private final Object o = new Object();
+    private double heading;
+    private Thread threadIMU;
 
+    public synchronized double getHeading(){
+        synchronized (o) {
+            return heading;
+        }
+    }
 
-    public DumbIMU(HardwareMap hm){
+    public DumbIMU(HardwareMap hm, LinearOpMode op){
         imu = hm.get(IMU.class, "imu");
         RevHubOrientationOnRobot.LogoFacingDirection logoDirection = RevHubOrientationOnRobot.LogoFacingDirection.LEFT;
         RevHubOrientationOnRobot.UsbFacingDirection  usbDirection  = RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD;
 
         this.orientationOnRobot = new RevHubOrientationOnRobot(logoDirection, usbDirection);
-    }
 
-    public void init(){
-        imu.initialize(new IMU.Parameters(orientationOnRobot));
-    }
-
-    @Override
-    public void atStart() {
 
     }
 
-    @Override
-    public void loop() {
-        YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
-        heading = orientation.getYaw(AngleUnit.RADIANS);
+    private void newThread(LinearOpMode op){
+        threadIMU = new Thread(()->{
+            imu.initialize(new IMU.Parameters(orientationOnRobot));
+            while(op.opModeIsActive() && !op.isStopRequested()){
+                synchronized (o){
+                    YawPitchRollAngles orientation = imu.getRobotYawPitchRollAngles();
+                    heading = orientation.getYaw(AngleUnit.RADIANS);
+                }
+            }
+        });
+        threadIMU.start();
+    }
+
+    public void init(LinearOpMode op){
+        if (threadIMU==null)newThread(op);
+        else if(threadIMU.isAlive()){
+            threadIMU.interrupt();
+            newThread(op);
+        }
     }
 }
